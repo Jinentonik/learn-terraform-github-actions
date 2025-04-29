fd59f6338f15169d05e21e79696cb2cc30db44a9
Jin Kang Tan <jinkang.tan@xtremax.com>
2025-04-29 15:50:14 +0800

    create second websg

--- main.tf
+++ main.tf
@@ -88,6 +88,22 @@ resource "aws_security_group" "web-sg1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_security_group" "web-sg2" {
+  name = "test-web-sg2"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
